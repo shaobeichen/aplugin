828706ae947f381b520b33826428faec096a9c73
shaobeichen <shaobeichen@outlook.com>
2024-11-13 16:20:23 +0800

    feat: add test example

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,23 @@
-import { sum } from './index'
+import { betaCalc } from './index'
 
-describe('sum function', () => {
-  it('should return the correct sum of two numbers', () => {
-    expect(sum(1, 2)).toBe(3)
-    expect(sum(-1, 1)).toBe(0)
-    expect(sum(-1, -1)).toBe(-2)
+describe('plugin flow', () => {
+  it('plugin base test', () => {
+    betaCalc.setValue(3)
+    expect(betaCalc.currentValue).toBe(3)
+    betaCalc.press('plus', 2)
+    expect(betaCalc.currentValue).toBe(5)
+    betaCalc.press('minus', 4)
+    expect(betaCalc.currentValue).toBe(1)
+  })
+  it('plugin base test', () => {
+    const name = 'squared'
+    const squaredPlugin = {
+      name,
+      exec: (currentValue: number) => currentValue * currentValue,
+    }
+    betaCalc.register(squaredPlugin)
+    betaCalc.setValue(5)
+    betaCalc.press<typeof name>(name)
+    expect(betaCalc.currentValue).toBe(25)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-export function sum(a: number, b: number): number {
-  return a + b
+const core = {
+  plus: (currentVal: number, addend: number) => currentVal + addend,
+  minus: (currentVal: number, subtrahend: number) => currentVal - subtrahend,
+} as const
+
+export const betaCalc = {
+  currentValue: 0,
+
+  setValue(value: number) {
+    this.currentValue = value
+  },
+
+  core,
+
+  plugins: {},
+
+  press<T>(buttonName: keyof typeof core | T, newVal?: number) {
+    const func = this.core[buttonName] || this.plugins[buttonName]
+    this.setValue(func(this.currentValue, newVal))
+  },
+
+  register(plugin: { name: string; exec: (currentVal: number) => number }) {
+    const { name, exec } = plugin
+    this.plugins[name] = exec
+  },
 }
